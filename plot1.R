5d6e2137616906267e179b1e5f2207091888263c
annamd <anna.dudel@gmail.com>
2017-04-20 19:42:14 +0200

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -5,20 +5,13 @@ dane0<-read.table(file="household_power_consumption.txt", sep=";", header=TRUE,
                 "numeric", "numeric", "numeric"))
 
 #converting date and time variables to appropriate format
-head(dane0)
-class(dane0$Date)
 dane0[,1]<-as.Date(dane0[,1], format="%d/%m/%Y")
-head(dane0)
 
 #choosing only data referring to the period to be analysed
 dane1<-subset(dane0, Date=="2007-02-01" | Date=="2007-02-02")
 dane1$Date_time=as.character(paste(dane1$Date, dane1$Time))
-dane1$Date_time
-head(dane1)
 dane1$dt<-strptime(dane1$Date_time, format="%Y-%m-%d %H:%M:%S")
-head(dane1)
-class(dane1$Date_time)
-class(dane1$dt)
+
 
 #histogram of global active power
 #to be saved in png file
--- plot4.R
+++ plot4.R
@@ -5,20 +5,14 @@ dane0<-read.table(file="household_power_consumption.txt", sep=";", header=TRUE,
                                "numeric", "numeric", "numeric"))
 
 #converting date and time variables to appropriate format
-head(dane0)
-class(dane0$Date)
 dane0[,1]<-as.Date(dane0[,1], format="%d/%m/%Y")
-head(dane0)
 
 #choosing only data referring to the period to be analysed
 dane1<-subset(dane0, Date=="2007-02-01" | Date=="2007-02-02")
 dane1$Date_time=as.character(paste(dane1$Date, dane1$Time))
-dane1$Date_time
-head(dane1)
+
 dane1$dt<-strptime(dane1$Date_time, format="%Y-%m-%d %H:%M:%S")
-head(dane1)
-class(dane1$Date_time)
-class(dane1$dt)
+
 
 #plot4
 Sys.setlocale("LC_TIME", "English")
@@ -42,7 +36,8 @@ lines(dane1$dt, dane1$Sub_metering_2, type="l", xlab="",
 lines(dane1$dt, dane1$Sub_metering_3, type="l", xlab="",
       ylab="Energy sub metering", col="blue")
 legend("topright", lty=1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       y.intersp=0.5, cex=0.75)
 
 #bottom right
 plot(dane1$dt, dane1$Global_reactive_power, type="l", xlab="datetime",
